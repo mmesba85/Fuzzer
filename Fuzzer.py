8642b728b31d82551919dad2a77da2366a50530b
Maroua Mesbahi <maroua.nebiya@gmail.com>
2019-07-06 15:21:29 +0200

    Fuzzer: entry point

--- Fuzzer.py
+++ Fuzzer.py
@@ -1,96 +1,43 @@
-#!/usr/bin/python3
-import socket
-import select
-import logging
-import Generator
-import http.client
 import argparse
+import logging
+import Worker
+import Generator
 
-def http_fuzz(http_headers, host="127.0.0.1", port="8080"):
-    req = "{} {} HTTP/{}\r\n".format(http_headers['Method'], http_headers['Path'], http_headers['Version'])
-    for k, v in http_headers.items():
-        if k == 'Method' or k == 'Path' or k == 'Version' or k == 'Data':
-            continue
-        req += k
-        req += ": "
-        req += v
-        req += "\r\n"
+# MAIN 
 
-    req += "\r\n"
+# NOT FINISHED
+parser = argparse.ArgumentParser()
+parser.add_argument("-H", type=str,
+                    help="Host to fuzz", default='127.0.0.1')
+parser.add_argument("-P", type=str,
+                    help="Port that listens")
+parser.add_argument("-T", type=str,
+                    help="Protocol", choices=["FTP", "HTTP"], required=True)
+parser.add_argument("-f", type=str,
+                    help="Config file")
+parser.add_argument("-l", type=str,
+                    help="Log file", default='fuzz.log')
+args = parser.parse_args()
 
-    if http_headers['Method'] == "POST":
-        req += http_headers[data]
-
-    return req
-
-def send_http_request(req, host, port, filename):
-    d = {'action': 'SEND REQ', 'host': host, 'protocol':'HTTP'}
-    logger.debug(req, extra=d)
-    sock = socket.create_connection((host, int(port)))
-    sock.setblocking(0)
-    sock.send(req.encode())
-    ready = select.select([sock], [], [], 1)
-    d = {'action': 'RESPONSE', 'host': host, 'protocol':'HTTP'}
-    if ready[0]:
-        ans = sock.recv(1024)
-        logger.debug(ans, extra=d)
+if args.P is None:
+    if args.T == 'FTP':
+        args.P = 21
     else:
-        logger.error('Empty Response', extra=d)
-    sock.close()
+        args.P = 8080
 
-def ftp_fuzzs(command, data, username="", passw="", host="127.0.0.1", port="21"):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    c = s.connect(host, int(port))
-    rec = s.recv(1024)
-
-    req = command + ' ' + data + '\r\n'
-    d = {'action': 'SEND REQ', 'host': host, 'protocol':'FTP'}
-    logger.debug(req, extra=d)
-
-    if command == 'USER':
-        s.send(req)
-        res = s.recv(1024)
-        process_response('FTP', host, res)
-        s.close()
-        return 0
-    
-    s.send('USER ' + username + '\r\n')
-    res = s.recv(1024)
-
-    elif command == 'PASS':
-        s.send(req)
-        res = s.recv(1024)
-        process_response('FTP', host, res)
-        s.close()
-        return 0
-
-    s.send('PASS ' + passw + '\r\n')
-    res = s.recv(1024)
-
-    s.send(req)
-    res = s.recv(1024)
-    process_response('FTP', host, res)
-
-def process_response(protocol, host, res):
-    d = {'action': 'RESPONSE', 'host': host, 'protocol':protocol}
-    if len(res) == 0:
-        logger.debug("Empty Response", extra=d)
+if args.f is None:
+    if args.T == 'FTP':
+        data = Generator.get_random_string()
+        # appel a la methode principale de fuzz
     else:
-        logger.debug(res, extra=d)
-    s.close()
+        http_headers = Generator.generate_http_input()
+        Worker.http_fuzz(http_headers, args.H, args.P) #provisoire
 
 logger = logging.getLogger()
-handler = logging.FileHandler('fuzz.log')
+handler = logging.FileHandler(args.l)
 formatter = logging.Formatter(
         '%(asctime)s %(protocol)-12s %(host)-12s %(action)-8s %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
 
-
-
-'''
-dic = Generator.generate_http_input()
-req = http_fuzz(dic)
-send_request(req, '127.0.0.1', 8080, 'toto')
-'''
